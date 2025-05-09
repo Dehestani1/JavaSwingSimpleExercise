3ba5eedca6ff2bf7c88ef7f1f9bd7185eed7ff83
Dehestani <sdk99333@gmail.com>
2025-05-09 21:23:26 +0330

    Part 1 completed.

---
+++ part-1/E1.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class E1 extends JFrame implements ActionListener {
+    private JLabel counterLable;
+    private JButton clickButton;
+    private int counter;
+    public E1() {
+        setTitle("Click Counter");
+        counterLable = new JLabel("Clicks = 0", SwingConstants.CENTER);
+        setSize(500, 500);
+        clickButton = new JButton("Click");
+        clickButton.addActionListener(this);
+        setLayout(new BorderLayout());
+        add(counterLable, BorderLayout.CENTER);
+        add(clickButton, BorderLayout.SOUTH);
+    }
+    public void actionPerformed(ActionEvent e) {
+        counter++;
+        counterLable.setText("Clicks = " + counter);
+    }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()-> new E1().setVisible(true));
+    }
+}
